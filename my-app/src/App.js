2ad157b569ccbea78f669de217db07787095d6c9
Muneer Abrahams <audipunkwp@gmail.com>
2025-03-20 21:35:56 +0200

    my-app/src/blog-posts.png

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+import logo from './blog-posts.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="blog-posts" alt="blog" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Click <code>to view</code> Posts.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://jsonplaceholder.typicode.com/posts"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Posts
         </a>
       </header>
     </div>
